c379d3af6857cc6cf417bb4c2f545a6335c84d8b
Shunta Saito <shunta.saito@gmail.com>
2020-03-28 21:32:37 +0900

    Update burn.py

--- burn.py
+++ burn.py
@@ -3,7 +3,6 @@
 
 import argparse
 import math
-import os
 import pprint
 
 import imageio
@@ -14,85 +13,115 @@ from PIL import ImageFont
 from pycaption import SRTReader
 from tqdm import tqdm
 
-parser = argparse.ArgumentParser()
-parser.add_argument('--video', type=str)
-parser.add_argument('--srt', type=str)
-parser.add_argument('--font', type=str,
-                    default='/System/Library/Fonts/HelveticaNeue.ttc')
-parser.add_argument('--font-size', type=int, default=36)
-parser.add_argument('--bottom-space', type=int, default=36)
-parser.add_argument('--skip-first', action='store_true', default=False)
-args = parser.parse_args()
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--video', type=str)
+    parser.add_argument('--out', type=str)
+    parser.add_argument('--srt', type=str)
+    parser.add_argument('--font', type=str,
+                        default='/System/Library/Fonts/HelveticaNeue.ttc')
+    parser.add_argument('--font-size', type=int, default=36)
+    parser.add_argument('--font-index', type=int, default=10)
+    parser.add_argument('--font-italic-index', type=int, default=11)
+    parser.add_argument('--secondary-font-index', type=int, default=11)
+    parser.add_argument('--secondary-font-start', type=int, default=433)
+    parser.add_argument('--secondary-font-end', type=int, default=449)
+    parser.add_argument('--bottom-space', type=int, default=36)
 
-srt_text = open(args.srt).read()
-srt = SRTReader().read(srt_text, lang='en')
-captions = srt.get_captions('en')
+    parser.add_argument('--codec', type=str, default='prores_ks')
+    parser.add_argument('--pixelformat', type=str, default='yuv444p10le')
+    parser.add_argument(
+        '--quality', type=int, default=10,
+        help='The quality measure of the output video. It should be within 0 to 10.')
+    parser.add_argument(
+        '--skip-first', action='store_true', default=False,
+        help='If it is True, the first caption will be skipped.')
+    parser.add_argument(
+        '--break-after', type=float, default=0,
+        help='Enable test mode to finish after the given seconds.')
+    args = parser.parse_args()
 
-reader = imageio.get_reader(args.video)
-meta_data = reader.get_meta_data()
-pprint.pprint(meta_data)
-fps = meta_data['fps']
+    srt_text = open(args.srt).read()
+    srt = SRTReader().read(srt_text, lang='en')
+    captions = srt.get_captions('en')
 
-out_fn = os.path.splitext(args.video)[0] + '_sub.mp4'
-print('out_fn:', out_fn)
-writer = imageio.get_writer(
-    out_fn, fps=fps, pixelformat='yuv420p', macro_block_size=8, quality=10)
+    reader = imageio.get_reader(args.video)
+    meta_data = reader.get_meta_data()
+    pprint.pprint(meta_data)
+    fps = meta_data['fps']
 
-caption_data = []
-for caption in captions:
-    if len(caption_data) == 0 and args.skip_first:
-        args.skip_first = False
-        continue
-    start_sec = caption.start / 1000 / 1000
-    end_sec = caption.end / 1000 / 1000
-    start_frame_num = start_sec * fps
-    end_frame_num = end_sec * fps
-    text = caption.get_text()
+    print('save to:', args.out)
+    writer = imageio.get_writer(
+        args.out,
+        codec=args.codec,
+        fps=fps,
+        pixelformat=args.pixelformat,
+        macro_block_size=8,
+        quality=args.quality,
+    )
 
-    indent = False
-    if '<i>' in text:
-        text = text.replace('<i>', '')
-        text = text.replace('</i>', '')
-        indent = True
+    caption_data = []
+    for caption in captions:
+        if len(caption_data) == 0 and args.skip_first:
+            args.skip_first = False
+            continue
+        start_sec = caption.start / 1000 / 1000
+        end_sec = caption.end / 1000 / 1000
+        start_frame_num = start_sec * fps
+        end_frame_num = end_sec * fps
+        text = caption.get_text()
 
-    start_frame_num = int(math.floor(start_frame_num))
-    end_frame_num = int(math.ceil(end_frame_num))
+        italic = False
+        if '<i>' in text:
+            text = text.replace('<i>', '')
+            text = text.replace('</i>', '')
+            italic = True
 
-    caption_data.append({
-        'text': text,
-        'start_frame_num': start_frame_num,
-        'end_frame_num': end_frame_num,
-        'italic': True,
-    })
+        start_frame_num = int(math.floor(start_frame_num))
+        end_frame_num = int(math.ceil(end_frame_num))
 
+        caption_data.append({
+            'text': text,
+            'start_frame_num': start_frame_num,
+            'end_frame_num': end_frame_num,
+            'italic': italic,
+        })
 
-caption_i = 0
-pbar = tqdm(total=reader.count_frames())
-for frame_i, frame in enumerate(reader):
-    caption = caption_data[caption_i]
-    if caption['start_frame_num'] <= frame_i <= caption['end_frame_num']:
-        image = Image.fromarray(frame)
-        draw = ImageDraw.Draw(image)
-        draw.font = ImageFont.truetype(
-            args.font, args.font_size, 13 if caption['italic'] else 0,
-            layout_engine=ImageFont.LAYOUT_BASIC)
+    caption_i = 0
+    pbar = tqdm(total=reader.count_frames())
+    for frame_i, frame in enumerate(reader):
+        if caption_i < len(caption_data):
+            caption = caption_data[caption_i]
+        if caption['start_frame_num'] <= frame_i <= caption['end_frame_num']:
+            image = Image.fromarray(frame)
+            draw = ImageDraw.Draw(image)
+            if caption['italic']:
+                font_i = args.font_italic_index
+            else:
+                font_i = args.font_index
+            draw.font = ImageFont.truetype(
+                args.font, args.font_size, font_i,
+                layout_engine=ImageFont.LAYOUT_BASIC)
 
-        h, w, _ = frame.shape
-        wt, ht = draw.font.getsize_multiline(caption['text'])
+            h, w, _ = frame.shape
+            wt, ht = draw.font.getsize_multiline(caption['text'])
 
-        pos = [
-            int(w / 2 - wt / 2),  # center
-            h - ht - args.bottom_space  # bottom
-        ]
-        color = (255, 255, 255)
-        draw.text(pos, caption['text'], color, align='center', stroke_width=2, stroke_fill=(0, 0, 0))
-        writer.append_data(np.array(image))
-    elif frame_i > caption['end_frame_num']:
-        caption_i += 1
-    else:
-        writer.append_data(frame)
+            pos = [
+                int(w / 2 - wt / 2),  # center
+                h - ht - args.bottom_space  # bottom
+            ]
+            color = (255, 255, 255)
+            draw.text(pos, caption['text'], color, align='center',
+                      stroke_width=2, stroke_fill=(0, 0, 0))
+            writer.append_data(np.array(image))
+        elif frame_i > caption['end_frame_num']:
+            caption_i += 1
+        else:
+            writer.append_data(frame)
+        pbar.update(1)
 
-    pbar.update(1)
+        if args.break_after > 0 and (frame_i / fps) > args.break_after:
+            break
 
-reader.close()
-writer.close()
+    reader.close()
+    writer.close()
