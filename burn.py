b34f2d987a363d35129be757a6d41b515053730b
Shunta Saito <shunta.saito@gmail.com>
2020-03-29 18:58:57 +0900

    Add some options to burn.py

--- burn.py
+++ burn.py
@@ -13,6 +13,7 @@ from PIL import ImageFont
 from pycaption import SRTReader
 from tqdm import tqdm
 
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument('--video', type=str)
@@ -23,16 +24,22 @@ if __name__ == "__main__":
     parser.add_argument('--font-size', type=int, default=36)
     parser.add_argument('--font-index', type=int, default=10)
     parser.add_argument('--font-italic-index', type=int, default=11)
-    parser.add_argument('--secondary-font-index', type=int, default=11)
-    parser.add_argument('--secondary-font-start', type=int, default=433)
-    parser.add_argument('--secondary-font-end', type=int, default=449)
+    parser.add_argument('--secondary-font-index', type=int, default=10)
+    parser.add_argument('--secondary-font-italic-index', type=int, default=11)
+    parser.add_argument('--secondary-font-size', type=int, default=52)
+    parser.add_argument('--secondary-font-start', type=int, default=432)
+    parser.add_argument('--secondary-font-end', type=int, default=448)
     parser.add_argument('--bottom-space', type=int, default=36)
 
+    parser.add_argument('--blueback', action='store_true', default=False)
     parser.add_argument('--codec', type=str, default='prores_ks')
-    parser.add_argument('--pixelformat', type=str, default='yuv444p10le')
+    parser.add_argument('--pixelformat', type=str, default='yuv422p10le')
     parser.add_argument(
         '--quality', type=int, default=10,
         help='The quality measure of the output video. It should be within 0 to 10.')
+    parser.add_argument(
+        '--bitrate', type=int, default=None,
+        help='The bitrate in b/s of the output video. The default is None (--quality is used).')
     parser.add_argument(
         '--skip-first', action='store_true', default=False,
         help='If it is True, the first caption will be skipped.')
@@ -56,8 +63,9 @@ if __name__ == "__main__":
         codec=args.codec,
         fps=fps,
         pixelformat=args.pixelformat,
-        macro_block_size=8,
+        macro_block_size=20,
         quality=args.quality,
+        bitrate=args.bitrate,
     )
 
     caption_data = []
@@ -67,8 +75,8 @@ if __name__ == "__main__":
             continue
         start_sec = caption.start / 1000 / 1000
         end_sec = caption.end / 1000 / 1000
-        start_frame_num = start_sec * fps
-        end_frame_num = end_sec * fps
+        start_frame_num = int(start_sec * fps)
+        end_frame_num = int(end_sec * fps)
         text = caption.get_text()
 
         italic = False
@@ -86,20 +94,37 @@ if __name__ == "__main__":
 
     caption_i = 0
     pbar = tqdm(total=reader.count_frames())
+
     for frame_i, frame in enumerate(reader):
+        if args.blueback:
+            frame = np.ones((1080, 1920, 3), dtype=np.uint8)
+            frame = frame * np.array([[[0, 0, 255]]], dtype=np.uint8)
+
         if caption_i < len(caption_data):
             caption = caption_data[caption_i]
         if caption['start_frame_num'] <= frame_i <= caption['end_frame_num']:
             image = Image.fromarray(frame)
             draw = ImageDraw.Draw(image)
-            if caption['italic']:
-                font_i = args.font_italic_index
+            if args.secondary_font_start <= caption_i <= args.secondary_font_end:
+                if caption['italic']:
+                    font_i = args.secondary_font_italic_index
+                else:
+                    font_i = args.secondary_font_index
+
+                draw.font = ImageFont.truetype(
+                    args.font, args.secondary_font_size, font_i,
+                    layout_engine=ImageFont.LAYOUT_BASIC
+                )
             else:
-                font_i = args.font_index
-            draw.font = ImageFont.truetype(
-                args.font, args.font_size, font_i,
-                layout_engine=ImageFont.LAYOUT_BASIC
-            )
+                if caption['italic']:
+                    font_i = args.font_italic_index
+                else:
+                    font_i = args.font_index
+
+                draw.font = ImageFont.truetype(
+                    args.font, args.font_size, font_i,
+                    layout_engine=ImageFont.LAYOUT_BASIC
+                )
 
             h, w, _ = frame.shape
             wt, ht = draw.font.getsize_multiline(caption['text'])
@@ -121,6 +146,8 @@ if __name__ == "__main__":
 
         if args.break_after > 0 and (frame_i / fps) > args.break_after:
             break
+        
+        frame_i += 1
 
     reader.close()
     writer.close()
